debcdf5cc7ff583f9f6704665fc755648cf0c24f
iamprashantverma <prashant2k26@gmail.com>
2025-08-15 14:12:52 +0530

    user-signup topic configured

--- PMS-Server/User-Service/src/main/java/com/pms/userservice/configs/KafkaConfig.java
+++ PMS-Server/User-Service/src/main/java/com/pms/userservice/configs/KafkaConfig.java
@@ -9,7 +9,7 @@ public class KafkaConfig {
 
     // Topic name constant
     public static final String PASSWORD_RESET_TOPIC = "user.password-reset-requested";
-
+    public static final String USER_SIGNUP_EVENT = "user-signup";
     /**
      * Kafka Topic bean for password reset events.
      * Creates the topic with 2 partitions and a replication factor of 1.
@@ -17,5 +17,10 @@ public class KafkaConfig {
     @Bean
     public NewTopic passwordResetRequestedTopic() {
         return new NewTopic(PASSWORD_RESET_TOPIC, 2, (short) 1);
+    }
+
+    @Bean
+    public NewTopic userSignupRequestedTopic() {
+        return new NewTopic(USER_SIGNUP_EVENT, 2, (short) 1);
     }
 }
--- PMS-Server/User-Service/src/main/java/com/pms/userservice/producer/UserEventProducer.java
+++ PMS-Server/User-Service/src/main/java/com/pms/userservice/producer/UserEventProducer.java
@@ -1,6 +1,7 @@
 package com.pms.userservice.producer;
 
 import com.pms.userservice.events.PasswordResetRequestedEvent;
+import com.pms.userservice.events.UserSignupEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,14 @@ import static com.pms.userservice.configs.KafkaConfig.PASSWORD_RESET_TOPIC;
 @Service
 @RequiredArgsConstructor
 public class UserEventProducer {
-    private final KafkaTemplate<Long, PasswordResetRequestedEvent> kafkaTemplate;
+    private final KafkaTemplate<Long, PasswordResetRequestedEvent> resetRequestedEventKafkaTemplate;
+    private final KafkaTemplate<String, UserSignupEvent>signupEventKafkaTemplate;
 
     public void sendPasswordResetRequestedEvent(PasswordResetRequestedEvent passwordResetRequestedEvent) {
-        kafkaTemplate.send(PASSWORD_RESET_TOPIC,passwordResetRequestedEvent.getId(),passwordResetRequestedEvent);
+        resetRequestedEventKafkaTemplate.send(PASSWORD_RESET_TOPIC,passwordResetRequestedEvent.getId(),passwordResetRequestedEvent);
+    }
+
+    public void sendUserSignupEvent(UserSignupEvent user) {
+        signupEventKafkaTemplate.send()
     }
 }
